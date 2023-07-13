be986988972d4344ed1ba7d6cc43bdc01c800ba8
XenoverseUp <xenoverseup@gmail.com>
2023-07-13 03:03:11 +0300

    Local storage is fixed,.

--- components/hooks/useLocalStorage.js
+++ components/hooks/useLocalStorage.js
@@ -3,6 +3,8 @@
 import { useState, useEffect } from "react"
 
 const useLocalStorage = (storageKey, fallbackState) => {
+  if (typeof window == "undefined") return [fallbackState, null]
+
   const [value, setValue] = useState(
     JSON.parse(localStorage.getItem(storageKey)) ?? fallbackState,
   )
