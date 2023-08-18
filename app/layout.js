31e42eb245a7639d2834e94476e63c5f000ba9a5
XenoverseUp <xenoverseup@gmail.com>
2023-08-19 00:30:44 +0300

    refactor: revert to vaul

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,6 @@ export default function RootLayout({ children }) {
         <MainNavigation />
         {children}
         <MainFooter />
-        <div id="drawer"></div>
       </body>
     </html>
   )
