5ddfc4e9dbaf06ca64c2e27421c2f795943e1ce0
XenoverseUp <xenoverseup@gmail.com>
2023-08-21 13:07:27 +0300

    feat: feat: added cronjob to update cache daily

---
+++ app/api/cron/updateCache.js
@@ -0,0 +1,11 @@
+import Cache, { GUESTBOOK_COUNT } from "@/lib/cache"
+import { GUESTBOOK_REFERRER } from "@/lib/guestbook"
+import redis from "@/lib/redis"
+
+/**
+ * This method runs at 5AM everyday to update cache.
+ */
+export const GET = async () => {
+  console.log(`\n\nUPDATING CACHE...\n\n`)
+  Cache.set(GUESTBOOK_COUNT, await redis.llen(GUESTBOOK_REFERRER))
+}
